ddef45ea8533c85488b69a76b1367cef5ec0d458
Nocelli <rafaelnocelliifsp@gmail.com>
2020-06-03 02:26:07 -0300

    Added points end point for registering points

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ routes.get('/items', async (request, response) => {
     const items = await knex('items').select('*')
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://${process.env.HOST}:${process.env.PORT}/uploads/${item.image}`
         }
@@ -15,6 +16,45 @@ routes.get('/items', async (request, response) => {
 
     return response.json(serializedItems)
 })
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        lat,
+        lon,
+        city,
+        uf,
+        items
+    } = request.body
+
+    const transaction = await knex.transaction()
+
+    const insertedPointId = (await transaction('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        lat,
+        lon,
+        city,
+        uf
+    }))[0]
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id: insertedPointId
+        }
+    })
+
+    await transaction('point_items').insert(pointItems)
+
+    return response.status(201).json({ sucess: true })
+})
+
+
 
 routes.get('/', (request, response) => {
     return response.json('Hello World!')
