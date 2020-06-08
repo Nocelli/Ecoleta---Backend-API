d452f4f73e3157bc69e1245e855cffe980559166
Nocelli <rafaelnocelliifsp@gmail.com>
2020-06-07 22:11:36 -0300

    Added validation for point creation

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { celebrate, Joi } from 'celebrate'
 
 import multer from 'multer'
 import multerConfig from './config/multer'
@@ -14,9 +15,28 @@ const itemsController = new ItemsController()
 
 routes.get('/items', itemsController.Index)
 
-routes.post('/points',upload.single('image') ,pointsController.create)
-routes.get('/points',pointsController.index)
-routes.get('/points/:id',pointsController.show)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
+routes.post(
+    '/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().email().required(),
+            whatsapp: Joi.number().required(),
+            lat: Joi.number().required(),
+            lon: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().max(2).required(),
+            items: Joi.string().required(),
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create
+)
+
 
 
 export default routes
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import routes from './routes'
 import cors from 'cors'
 import path from 'path'
 import dotenv from 'dotenv'
+import { errors } from 'celebrate'
 
 dotenv.config({
     path: './.env'
@@ -14,7 +15,7 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
-
+app.use(errors())
 
 app.listen(process.env.PORT, () => {
     console.log('\x1b[32m', '\n\n\n\t\t\tServer starting!\n', "\x1b[33m", 'Port: ' + process.env.PORT)
